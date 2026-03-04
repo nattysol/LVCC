8825bc54c8aa636bb1905d01cc7ac0cfebe6e55b
Natural Solutions <kyle@naturalsolutions.llc>
2026-03-04 12:10:53 -0800

    Fix: Align migration command with container WORKDIR

--- medusa-config.js
+++ medusa-config.js
@@ -1,5 +1,7 @@
 const { loadEnv } = require("@medusajs/framework/utils")
+const path = require("path")
 
 loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
+// This tells the CLI that the real config is the .ts file next to it
 module.exports = require("./medusa-config.ts")
