3f53e7fb629e7c26fc2ebfe63b7a4e65cea3ba8d
Natural Solutions <kyle@naturalsolutions.llc>
2026-03-04 09:56:03 -0800

    Standardize: Move Medusa backend to root for cloud compatibility

--- medusa-config.js
+++
@@ -1,8 +0,0 @@
-const { loadEnv, defineConfig } = require("@medusajs/framework/utils")
-const path = require("path")
-
-// Load the environment variables from the backend folder
-loadEnv(process.env.NODE_ENV || "development", path.resolve(__dirname, "backend"))
-
-// Export the configuration from the backend folder
-module.exports = require("./backend/medusa-config")
