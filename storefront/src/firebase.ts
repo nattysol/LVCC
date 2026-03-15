b1700704f72391edc2c45edd614630fa1afb866d
Natural Solutions <kyle@naturalsolutions.llc>
2026-03-15 07:50:17 -0700

    firebase name correction

--- storefront/src/firebase.ts
+++ storefront/src/firebase.ts
@@ -6,6 +6,6 @@ import firebaseConfig from "../firebase-applet-config.json";
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app, "ai-studio-c79b7a1a-9645-424a-bb95-38651e3979a2");
 
 export default app;
